093fe19cde7718ec100e98f644efa41003bb737b
Deep Pudasaini <deep.pudasaini19@my.northampton.ac.uk>
2022-08-05 10:03:19 +0545

    Splash fix

--- src/pages/splash/Splash.js
+++ src/pages/splash/Splash.js
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
 import "./Splash.css";
 import { Redirect } from "react-router-dom";
-import LoaderLogo from "../../components/Loader/LoaderLogo.js";
+//import LoaderLogo from "../../components/Loader/LoaderLogo.js";
 
 function AnimatedSplash(props) {
   return (
     <div className="logo_wrapper">
       <div className="screen" style={{ backgroundColor: props.theme.text }}>
-        <LoaderLogo id="logo" theme={props.theme} />
+
       </div>
     </div>
   );
